e7269ff66d3445f72d8a72d1e48f61cc40604531
Darnster <danny.ruttle@gmail.com>
2023-02-19 17:42:43 +0000

    aws_lambda stable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 from bs4 import BeautifulSoup
 import requests
 import sys, os
@@ -7,12 +8,11 @@ import time
 import calendar
 import hashlib
 import email, smtplib, ssl
-import CryptProcess # for gmail password encryption
 import cfg_parser
 
 __author__ = "danny.ruttle@gmail.com"
-__version__ = "1.1"
-__date__ = "12-02-2023"
+__version__ = "2.1"
+__date__ = "19-02-2023"
 
 """
 Credit to:  https://www.pluralsight.com/guides/web-scraping-with-beautiful-soup
@@ -23,41 +23,28 @@ It queries the Florida launch timetable page (https://floridareview.co.uk/things
 the results in an easy to read summary as a table in an HTML page.
 
 It then runs periodically using crond and uses md5 to determine whether the page has been updated and emails the output.
-.||.... may publish to a location on my phone instead???
+.... may publish to a location on my phone instead???
 
-Features Complete
+Features Complete (beyond version 1.0/1.1)
 -----------------
-1. normalise dates to compare with sysdate DONE
-2. html + css for output
-3. hash the response to see if it has changed before generating the output (hash stored in (signature.txt))
-4. logging of the result of each run of the script
-5. email output to me when something changes - possibly configurable
-6. encrypt app password for gmail
-7. HTML now published to body of email
-8. Working directory provided for config, signature and log files
-9. Working directory path separators corrected (bug #003).
-10. run_log.txt output updated to reduce verbosity and only show last 4 chars of the digest,
-11. Schedule in cron on my phone using "* * * * * cd <working directory> && python main.py"
-12. Update gmail account to rocket.spotter@gmail.com.
-13. Constrain the current_time comparison in generate_output() to be rounded down to 0:00 on that date (whole day).
-14. Email addresses for recipients is configurable
-15. The script now emails the log after each run to rockets.spotter@gmail.com (v 1.1)
-
-
-Bugs fixed:
-#001 Source page added a descriptive date with a comma
-#002 lack of path to working directory causing cron to fail
-#003 slashes to working directory where \\ need to be /
+1. Modified log to insert into DynamoDB table
+2. Checked over minor differences between 1.0 and 1.1 and update (notifications elements).
+3. AWS cron is in place
 
 
 TO DO
 -----
-.
+1. Improve formatting on tables
+2. Add link to youtube channels
+
+MUCH LATER
+----------
+1. Plug password crypto back in (_rust import issue with this so app pwd now in the code)
 
 """
 
-# global constants
 working_directory = os.getcwd()
+
 
 def process():
     """
@@ -67,20 +54,19 @@ def process():
 
     config_dict = read_config("config.txt")
     # encrypted credentials for sending output via gmail
-    pwd = config_dict.get("pwd")
-    key = config_dict.get("key")
     recipients = config_dict.get("recipients")
 
+    # override bot protection on floridaview site
+    headers = {'User-Agent': 'SpaceSchedule'}
+    content = requests.get("https://floridareview.co.uk:443/things-to-do/current-launch-schedule", headers=headers)
 
-    content = requests.get("https://floridareview.co.uk/things-to-do/current-launch-schedule")
-    # print(content)
     soup = BeautifulSoup(content.text, 'html.parser')
     tags = soup.find_all(['h2', 'p'])  # Extract and return first occurrence of h2
     #  print(tags)  # Print row with HTML formatting
     if check_page_update(tags):  # see if digest of tags array is different
         # need to build an array of arrays here, e.g. [[date],mission_details,launch pad, link] link -> #march72023-spacexfalcon9intelsat40e
         missions_array = []
-        ctrl_flag = False #controls when the process stops appending to the mission array (this_mission)
+        ctrl_flag = False  # controls when the process stops appending to the mission array (this_mission)
         this_mission = []
         for tag in tags:
             if ctrl_flag == False:
@@ -89,9 +75,8 @@ def process():
                 mission = process_h2(tag)
                 if mission:
                     this_mission = mission
-                    #print(this_mission)
+                    # print(this_mission)
                     ctrl_flag = True
-
 
             if ctrl_flag == True:
                 if tag.get_text()[0:6] == "Launch":
@@ -100,11 +85,7 @@ def process():
                     ctrl_flag = False
 
         output_string = generate_output(missions_array)
-
-        fh = open("%s/space_launch.html" % working_directory, "w")
-        fh.write(output_string)
-        fh.close()
-        notify_update(key, pwd, output_string, recipients)
+        notify_update(output_string, recipients)
 
 
 def read_config(config):
@@ -117,21 +98,23 @@ def read_config(config):
     return cp.read(config)
 
 
-
-def notify_update(key,pwd, output, recipients):
+def notify_update( output, recipients):
     from email import encoders
     from email.mime.base import MIMEBase
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
 
-    gpwd = CryptProcess.decrypt(bytes(pwd, 'utf-8'), key).decode()
+    # gpwd = 'mpdndvqvvrgrwwcs'
+    config_dict = read_config("config.txt")
+    # encrypted credentials for sending output via gmail
 
-    # print(recipients)
+    gpwd = 'mpdndvqvvrgrwwcs'
+
     subject = "Rocket Schedule Update"
     text = "An update has been made to the page - exciting!"
     sender_email = "rockets.spotter@gmail.com"
     receiver_email = str.split(recipients, ",")
-    password = gpwd # this is a google app password - uses pycrypto for this - see manual amend!
+    password = gpwd  # this is a google app password - uses pycrypto for this - see manual amend!
 
     # Create a multipart message and set headers
     message = MIMEMultipart("alternative")
@@ -140,8 +123,7 @@ def notify_update(key,pwd, output, recipients):
     message["Subject"] = subject
     message["Bcc"] = "rockets.spotter@gmail.com"  # Recommended for mass emails
 
-
-    html = output[15:] # removes doctype declaration
+    html = output[15:]  # removes doctype declaration
 
     # Turn these into plain/html MIMEText objects
     part1 = MIMEText(text, "plain")
@@ -159,41 +141,75 @@ def notify_update(key,pwd, output, recipients):
         server.sendmail(sender_email, receiver_email, message.as_string())
 
 
-
 def check_page_update(tags):
     """
     Generate a signature for incoming data and compare with the last version
     :param tags: multidemensional array of data returned from the web page
     :return: Boolean
     """
+
     sig = hashlib.shake_128()
     sig.update(str.encode(str(tags)))
     sig = sig.hexdigest(32)
     # print(sig)
     # now compare
-    sig_path = '%s/signature.txt' % working_directory
-    last_signature = open(sig_path, "r")
-    old_sig = last_signature.readline()
-    last_signature.close()
-    # print(old_sig)
+
+    # DynamoDB stuff here
+    dynamodb = boto3.resource('dynamodb',
+                              region_name='eu-west-2',
+                              endpoint_url='http://dynamodb.eu-west-2.amazonaws.com')
+    table = dynamodb.Table('RocketsDigest')
+
+    DigestQuery = table.scan()  # will only ever have one row in it
+
+    # add lndex error exception here for empty table
+
+    Digest = DigestQuery["Items"][0]["Digest"]
+
+    old_sig = Digest
+
     if sig == old_sig:
         # do nothing
-        log_run(sig, "did not update")
+        log_run(sig, "did not update", dynamodb)
         return False
     else:
-        new_sig = open(sig_path, "w")
-        new_sig.write(sig)
-        new_sig.close()
-        log_run(sig, "updated")
+
+        # delete existing Digest (tested and works):
+        scan = table.scan()
+        with table.batch_writer() as batch:
+            for each in scan['Items']:
+                batch.delete_item(
+                    Key={
+                        'Digest': each['Digest']
+                    }
+                )
+
+        # Insert new digest
+        response = table.put_item(
+            Item={
+                'Digest': sig, }
+        )
+
+        status_code = response['ResponseMetadata']['HTTPStatusCode']
+        print(status_code)
+
+        log_run(sig, "updated", dynamodb)
         return True
 
 
-def log_run(sig, action):
-    logfile = open("%s/run_log.txt" % working_directory, "a")
-    date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
-    output = "Script ran at %s and %s the output, digest was %s\n" % (date_string, action, sig[-4:])
-    logfile.write(output)
+def log_run(sig, action, dynamodb):
 
+    date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
+    output = "Script ran at %s and %s the output, digest was %s" % (date_string, action, sig[-4:])
+
+    table = dynamodb.Table('RocketsLog')
+    response = table.put_item(
+        Item={
+            'Signature': sig,
+            'Output' : output,
+            'LogTime' : date_string
+        }
+    )
     # now email the log to rockets.spotter@gmail.com
     notify_log(output)
 
@@ -209,7 +225,7 @@ def notify_log(output):
     pwd = config_dict.get("pwd")
     key = config_dict.get("key")
 
-    gpwd = CryptProcess.decrypt(bytes(pwd, 'utf-8'), key).decode()
+    gpwd = 'mpdndvqvvrgrwwcs'
 
     # ***********************************  need to look at email mime method, etc... ****************
     subject = "Rockets Run Log"
@@ -224,7 +240,6 @@ def notify_log(output):
     message["To"] = "rockets.spotter@gmail.com"
     message["Subject"] = subject
     message["Bcc"] = "rockets.spotter@gmail.com"  # Recommended for mass emails
-
 
     html = output[15:] # removes doctype declaration
 
@@ -252,7 +267,7 @@ def generate_output(missions_array):
 
     reference:  https://docs.python.org/3/library/time.html
     """
-    #print(missions_array)
+    # print(missions_array)
     date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
     html_head = """<!DOCTYPE html><html><head>
     <meta charset="utf-8" />
@@ -300,25 +315,23 @@ def generate_output(missions_array):
     [[['2022', '12', '31'], 'SpaceX Falcon 9, Transporter 6', 'january32023-spacexfalcon9transporter6', 'Launch was from launch pad SLC-40 with a launch time of 9:56 a.m. EST.'],...
     """
     table_detail_string = ""
-    current_day = (int(time.time() // 86400)) * 86400   # output = 6614352000
+    current_day = (int(time.time() // 86400)) * 86400  # output = 6614352000
 
     for mission in missions_array:
         # print(mission)
-        #first assign any null days (no date defined in schedyule) to 1st of month for comparison only
+        # first assign any null days (no date defined in schedyule) to 1st of month for comparison only
         if mission[0][2] == "null":
             mission_day = 1
         else:
             mission_day = mission[0][2]
         # print(mission_day)
-        mission_date = datetime(int(mission[0][0]),int(mission[0][1]),int(mission_day),0,0)
+        mission_date = datetime(int(mission[0][0]), int(mission[0][1]), int(mission_day), 0, 0)
         mission_date = calendar.timegm(mission_date.timetuple())
         if mission_date >= current_day:
-            #print("mission_date = %s, current_time = %s" % (mission_date, current_time))
+            # print("mission_date = %s, current_time = %s" % (mission_date, current_time))
             mission_row_string = create_mission_row(mission)
             table_detail_string += mission_row_string
     return html_head + table_head + table_detail_string + table_footer + html_footer
-
-
 
 
 def create_mission_row(mission):
@@ -346,9 +359,6 @@ def create_mission_row(mission):
     return row_string
 
 
-
-
-
 def process_h2(tag):
     """
     Get date in a form where it can be compared with sysdate
@@ -358,34 +368,34 @@ def process_h2(tag):
     :param tag:
     :return: array [[<date>], <mission>, <taganchor>, <Launch Pad>]
     """
-    month_dict = {# used to pad leading zero
-        "January" : "01",
-        "February" : "02",
-        "March" : "03",
-        "April" : "04",
-        "May" : "05",
-        "June" : "06",
-        "July" : "07",
-        "August" : "08",
-        "September" : "09",
-        "October" : "10",
-        "November" : "11",
-        "December" : "12"
+    month_dict = {  # used to pad leading zero
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
     }
 
     details = tag.get_text()
     # Need to make sure we are dealing with a date
-    if details[0:3] in ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']:
-        #print(details)
+    if details[0:3] in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']:
+        # print(details)
 
         """
         January 3, 2023 - SpaceX Falcon 9, Transporter 6
         March 2023 - SpaceX Falcon 9, Polaris Dawn
         ['March 2023', 'SpaceX Falcon 9, Polaris Dawn']
         ['March 2023']
-        
+
         bug #001 fix to this section.  Needed to remove processing of commas from date portion of the h2 tag as these are not uniformally structured 
-        
+
         # 'January 3, 2023 - SpaceX Falcon 9, Transporter 6'
         # or 'March 2023 - SpaceX Falcon 9, Polaris Dawn'
         # or 'February, 2023 - Relativity Space Terran 1, Good Luck, Have Fun'
@@ -407,7 +417,7 @@ def process_h2(tag):
             month = month_dict.get(date_array[0])
 
 
-        else: # 'January 2023'
+        else:  # 'January 2023'
             # ['January','2023']
             year = date_array[1]
             # returns '2023'
@@ -419,7 +429,6 @@ def process_h2(tag):
         return [[year, month, day], human_date, mission_string, anchor]
     else:
         pass
-
 
 
 if __name__ == "__main__":
